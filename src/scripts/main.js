5500bc63c00da8739072ef552b3c80174393b806
Anatoliy Korotenko <tolik.korotenko@21gmail.com>
2025-07-01 13:37:57 +0300

    solution-2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const link = event.target.closest('a.list-item__link');
+
+  if (!link) {
+    return;
+  }
+
+  largeImg.src = link.href;
+  largeImg.alt = link.title || 'Large image';
+});
